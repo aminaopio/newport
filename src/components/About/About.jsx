bfff5e6e6a1f5ff681bcca5f4842bfcfc836ac9c
Amina <aminaopio@my.unt.edu>
2023-10-24 20:46:37 -0500

    Fixed unique path error

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
+import { BiLogoHtml5, BiLogoJavascript, BiSolidFileCss } from 'react-icons/bi';
 import { BsFileArrowDown } from 'react-icons/bs';
-import { RiCss3Fill, RiHtml5Fill, RiJavascriptLine } from 'react-icons/ri';
 import { SiAmazonaws, SiAmazondynamodb, SiAngularjs, SiFirebase, SiFlutter, SiMysql } from 'react-icons/si';
 import Sidebar from "../../sidebar/Sidebar";
 import "./About.scss";
@@ -40,7 +40,7 @@ function About() {
       >
         <motion.h2 variants= {textVariants}>Hello!</motion.h2>
         <motion.h1 variants= {textVariants}>My name is Amina Opio and I'm an aspiring software engineer and recent graduate from the University of North Texas with a Bachelor of Science in Computer Science and a minor in International Studies. This is my personal website created with React, SASS and Framer Motion. Below you'll find some of my projects! 😁</motion.h1>
-        <motion.h3 variants= {textVariants}>Frontend Stacks: <RiJavascriptLine/>, <RiHtml5Fill/>, <RiCss3Fill/>, <SiFlutter/>,<SiAngularjs/>, etc. </motion.h3>
+        <motion.h3 variants= {textVariants}>Frontend Stacks: <BiLogoJavascript/>, <BiLogoHtml5/>, <BiSolidFileCss/>, <SiFlutter/>,<SiAngularjs/>, etc. </motion.h3>
         <motion.h3 variants= {textVariants}>Backend Stacks: <SiFirebase/>, <SiAmazonaws/>, <SiAmazondynamodb/>, <SiMysql/>, etc. </motion.h3>
         <motion.div className="scrollButton" variants= {textVariants} animate="scrollButton">
             <a href={"#CV"}><BsFileArrowDown/></a>
